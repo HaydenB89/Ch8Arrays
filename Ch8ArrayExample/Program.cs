6efe18d85b51f63cb30ebc1c2c95dc2db836d018
hayden brown <haydenbrowncinci@gmail.com>
2022-02-01 16:18:58 -0500

    Add project files.

---
+++ Ch8ArrayExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ch8ArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = {1,4,6,27,31,32,63,65};
+
+            int sum = 0;
+            for(int idx = 0; idx < ages.Length; idx++)
+            {
+                sum += ages[idx];
+            }
+            Console.WriteLine($"sum is {sum}");
+        }
+    }
+}
