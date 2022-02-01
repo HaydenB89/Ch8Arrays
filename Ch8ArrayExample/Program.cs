8765caed9acb7fce11d2e0643e6c69dc4ae16773
hayden brown <haydenbrowncinci@gmail.com>
2022-02-01 16:28:13 -0500

    replaced original with foreach

--- Ch8ArrayExample/Program.cs
+++ Ch8ArrayExample/Program.cs
@@ -6,12 +6,24 @@ namespace Ch8ArrayExample
     {
         static void Main(string[] args)
         {
+            /*
             int[] ages = {1,4,6,27,31,32,63,65};
 
             int sum = 0;
             for(int idx = 0; idx < ages.Length; idx++)
             {
                 sum += ages[idx];
+            }
+            Console.WriteLine($"sum is {sum}");
+            */
+
+            //foreach loop *easier than the above statement
+            int[] ages = { 1, 4, 6, 27, 31, 32, 63, 65 };
+
+            int sum = 0;
+            foreach(int age in ages)
+            {
+                sum += age;
             }
             Console.WriteLine($"sum is {sum}");
         }
